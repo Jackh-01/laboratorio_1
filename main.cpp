0277efb354172345aec7dba5c6e228c7416c1859
jackh-01 <jackh.narvaez@udea.edu.co>
2021-03-22 17:57:11 -0500

    Creacion del menu
    añadi una pequeña seccion para poder ver el ejercicio o problema que se desea ver

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+char cont= 0;
+int num_n= 0;
+
+
+int main()
+{
+        cout << "\n Que le gustaria buscar ?" << endl;
+        cout << "\n 1) Ejercicios\n 2) Problemas\n" << endl;
+        cout << "--> "; cin >> cont;cout << "\n\n";
+
+    if (cont== char (49)){;
+        cout<<"Que ejercicio desea realizar\n"<< endl;
+        cout << "-->";
+        cin>> num_n;cout << endl;
+        switch (num_n) {
+        case 1:{
+                /* Escriba un programa que pida dos números A y B e imprima en pantalla el residuo
+                    de la división A/B.*/
+
+
+                }
+                        }
+    }
+    return 0;
+}
